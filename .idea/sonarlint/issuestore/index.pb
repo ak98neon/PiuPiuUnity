047dc3c1efd7d1ba0e4650cfeb080a0a476d9816
ak98neon <artem_kudria@epam.com>
2019-10-27 22:46:54 +0200

    Update map

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,11 @@
 +Assets/Scripts/Multiplayer/MultiListener.cs,5/b/5b65cab3fa9fcc896459bc389694889eb7dfe80d
 L
 .idea/codeStyles/Project.xml,6/e/6ecd6000a7b6f4a2884412ff19f74193ed089648
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+P
+ Assets/Prefabs/Iskra.prefab.meta,4/a/4a96bfc8ecbe8e69a2f4b3c0dbc94d5828b5e135
+J
+Assets/Scripts/Shooting.cs,d/b/db7eb80ed6a01427e1cb8df31796ea34c6b9d4a6
+N
+Assets/Scripts/StatusPlayer.cs,a/5/a5877120a421da3a651ea0ce4cb7e17ae757a15f
---
+++ Assets/Scripts/Action/PickUpItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    public GUIStyle guiStyle = new GUIStyle();
+    public Camera playerCamera;
+    public GameObject playerHand;
+
+    public bool isCanTakeItem;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Ray r = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+        if (Physics.Raycast(r, out hitInfo))
+        {
+            if (hitInfo.transform.CompareTag("Weapon"))
+            {
+                isCanTakeItem = true;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    var weapon = hitInfo.transform.gameObject;
+                    Destroy(hitInfo.transform.gameObject);
+                    playerHand.transform.parent = weapon.transform;
+                    playerHand.transform.position = Vector3.zero;
+                    playerHand.transform.rotation = Quaternion.identity;
+
+                    isCanTakeItem = false;
+                }
+            }
+        } else
+        {
+            isCanTakeItem = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isCanTakeItem)
+        {
+            guiStyle.normal.textColor = Color.black;
+            guiStyle.fontSize = 20;
+            GUI.Label(new Rect(playerCamera.pixelWidth / 2, playerCamera.pixelHeight / 2 + 200, 150, 30), "For take push F", guiStyle);
+        }
+    }
+}
